0fcc7ecea9deeae4df66ec3983ab0d83ceb22daf
Jonathan Pereira <jonathan.phoenix1@gmail.com>
2020-08-01 20:51:37 +0530

    Revert "A"
    This reverts commit a211972acaf9c6d1eea2e534510aa211ad6877ff.

--- src/config/index.js
+++ src/config/index.js
@@ -7,7 +7,7 @@ module.exports = {
   siteUrl: 'https://jonathanpereira.dev',
   siteLanguage: 'en_US',
   googleAnalyticsID: 'UA-143014926-2',
-  //googleVerification: 'ehaLJDqegjHX8oa25889VmMcogvm579Q2_KHY7jIUQI',
+  //googleVerification: 'DCl7VAf9tcz6eD9gb67NfkNnJ1PKRNcg8qQiwpbx9Lk',
   name: 'Jonathan Pereira',
   location: 'Bengaluru',
   email: 'hello@jonathanpereira.dev',
--- src/pages/archive.js
+++ src/pages/archive.js
@@ -99,6 +99,8 @@ const ArchivePage = ({ location, data }) => {
   return (
     <Layout location={location}>
       <Helmet>
+        <title>Archive | Brittany Chiang</title>
+        <link rel="canonical" href="https://brittanychiang.com/archive" />
       </Helmet>
 
       <StyledMainContainer>
